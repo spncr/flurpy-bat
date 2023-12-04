1ddaf99c9c943b02e0b96d9e3f35657ebf235b21
spncr <spncr@weird.vision>
2023-12-04 15:37:35 -0800

    Game Over Polish
    put world nodes into @onready vars
    fixed some spacing
    removed game timer
    added animator

--- bat.gd
+++ bat.gd
@@ -3,6 +3,7 @@ extends Area2D
 signal died()
 
 var velocity = Vector2.ZERO
+var playing = false
 
 @export var grav = 1100
 @export var MAX_SPEED = 1200
@@ -11,32 +12,32 @@ var velocity = Vector2.ZERO
 @onready var sprite = $body
 
 func _process(delta):
-	velocity.y += grav * delta
-	
-	if Input.is_action_just_pressed("button"):
-		velocity.y -= power * delta
+	if playing:
+		velocity.y += grav * delta
+		
+		if Input.is_action_just_pressed("button"):
+			velocity.y -= power * delta
 
-	velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)
-	
-	var new_rotation:float = 0
-	if velocity.y < 0:
-		new_rotation = remap(velocity.y, -MAX_SPEED, 0, -PI/4, 0)
-	else:
-		new_rotation = remap(velocity.y, 0, MAX_SPEED, 0, PI)
+		velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)
+		
+		var new_rotation:float = 0
+		if velocity.y < 0:
+			new_rotation = remap(velocity.y, -MAX_SPEED, 0, -PI/4, 0)
+		else:
+			new_rotation = remap(velocity.y, 0, MAX_SPEED, 0, PI)
 
-	sprite.set_rotation(lerp_angle(sprite.rotation, new_rotation, delta * 30))
+		sprite.set_rotation(lerp_angle(sprite.rotation, new_rotation, delta * 30))
 
-	position += velocity * delta
-	if position.y >= 500:
-		velocity.y = 0
-	position.y = clampf(position.y, -100, 500)
+		position += velocity * delta
+		if position.y >= 500:
+			velocity.y = 0
+		position.y = clampf(position.y, -100, 500)
 
 func start(pos):
 	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+
 
 func _on_area_entered(area):
-	print(position.y)
 	velocity = Vector2.ZERO
+	playing = false
 	emit_signal("died") # Replace with function body.
--- world.gd
+++ world.gd
@@ -2,35 +2,80 @@ extends Node
 
 @export var obstacle_scene: PackedScene
 
+enum GameState {
+	READY,
+	GAME,
+	OVER
+}
+
+var game_state: GameState
+
 var score: int = 0
 var speed: float = 100
-var game_over: bool = false
+
+@onready var _ready_label := $"HUD/Ready?"
+@onready var _score_label := $"HUD/Score"
+@onready var _game_over_label := $"HUD/Game Over"
+@onready var _bat := $Bat
+@onready var _start_position: Vector2 = $StartPosition.position
+@onready var _animation_player := $AnimationPlayer
+@onready var _obstacle_timer := $ObstacleTimer
+@onready var _floor_stripe := $Floor/Stripe
 
 func _ready():
-	start_game()
+	_ready_label.visible = true
+	_score_label.visible = false
+	_game_over_label.visible = false
+	game_state = GameState.READY
+
 
 func _process(delta):
-	if game_over:
-		if Input.is_action_just_pressed("button"):
-			start_game()
-	else:
-		$Floor/Stripe.region_rect.position.x += (speed / 2 * delta)
+	match game_state:
+		GameState.READY:
+			_bat.start(_start_position)
+			move_floor(delta)
+			if Input.is_action_just_pressed("button"):
+				start_game()
+		GameState.GAME:
+			move_floor(delta)
+		GameState.OVER:
+			if Input.is_action_just_pressed("button") and not _animation_player.is_playing():
+				get_ready(delta)
+
+
+func get_ready(delta):
+	_animation_player.play("fade_to_black")
+	await _animation_player.animation_finished
+	_game_over_label.visible = false
+	get_tree().call_group("obstacles", "queue_free")
+	_ready_label.visible = true
+	_score_label.visible = false
+	_bat.start(_start_position)
+	move_floor(delta)
+	_animation_player.play_backwards("fade_to_black")
+	await _animation_player.animation_finished
+	game_state = GameState.READY
+
+
+func start_game():
+	_ready_label.visible = false
+	_bat.playing = true
+	_obstacle_timer.start()
+	score = 0
+	_score_label.text = str(score)
+	game_state = GameState.GAME
+
 
 func end_game():
-	$ObstacleTimer.stop()
+	game_state = GameState.OVER
+	_obstacle_timer.stop()
 	get_tree().call_group("obstacles", "stop_moving")
-	game_over = true
-	$"HUD/Game Over".visible = true
-	
-func start_game():
-	game_over = false
-	$"HUD/Game Over".visible = false
-	get_tree().call_group("obstacles", "queue_free")
-	$Bat.start($StartPosition.position)
-	$ObstacleTimer.start()
-	score = 0
-	$HUD/Score.text = str(score)
-	
+	_animation_player.play("game_over_descends")
+
+func move_floor(delta):
+	_floor_stripe.region_rect.position.x += (speed / 2 * delta)
+
+## Callbacks
 func _on_obstacle_timer_timeout():
 	var obstacle = obstacle_scene.instantiate()
 	obstacle.position.x = 500
@@ -40,9 +85,14 @@ func _on_obstacle_timer_timeout():
 	obstacle.scored.connect(_on_scored)
 	add_child(obstacle)
 
+
 func _on_bat_died():
 	end_game()
 
+
 func _on_scored():
-	if not game_over: score += 1
-	$HUD/Score.text = str(score)
+	if game_state == GameState.GAME:
+		score += 1
+		if score == 1:
+			_score_label.visible = true
+		_score_label.text = str(score)
